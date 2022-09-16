6082d86fa4cc5b662aa5d070d787c9556f9de82b
Timur Mustafin <mustafintf@eruditor-group.com>
2022-09-17 02:39:38 +0300

    task4 tree async

--- task4/tree.js
+++ task4/tree.js
@@ -1,17 +1,42 @@
 import {readdir} from 'fs/promises';
 
-async function writeByType(path, type){
-    return (await readdir(path, { withFileTypes: true })).filter(f => (type ==='dir' ? f.isDirectory() : !f.isDirectory()))
+async function* getFsItem(path) {
+    const items = (await readdir(path, {withFileTypes: true}))
+    const dirItems = items.filter(f => f.isDirectory())
         .map(f => f.name);
+    const fileItems = items.filter(f => (!f.isDirectory()))
+        .map(f => f.name);
+    for (const filePath of fileItems) {
+        yield {path: path + '/' + filePath, is_dir: false};
+    }
+
+    if (dirItems.length) {
+        for (const [index, value] of dirItems.entries()) {
+            if (index === 0) {
+                yield {path, is_dir: true};
+            }
+            yield* getFsItem(path + '/' + value);
+        }
+    } else {
+        yield {path, is_dir: true};
+    }
 }
 
 async function tree(path) {
     try {
-        const dirs = await writeByType(path, 'dir');
-        const files = await writeByType(path, 'files');
-        return {files: files, dirs: dirs};
+        const tree = {dir: [], files: []};
+
+        for await (const item of getFsItem(path)) {
+            if (item.is_dir) {
+                tree.dir.push(item.path);
+            } else {
+                tree.files.push(item.path);
+            }
+        }
+        return tree;
     } catch (err) {
         console.error(err);
     }
 }
-console.log(await tree(process.env.npm_config_path ?? ''));
+
+console.log(await tree(process.argv[2] ?? ''));
